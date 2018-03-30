81ef8afc6307b2a63510fff187c95156ac49f74f
ktso11 <sokatie11@gmail.com>
2018-03-30 14:08:36 -0700

    added more styling

--- src/components/City.js
+++ src/components/City.js
@@ -5,8 +5,11 @@ class City extends Component {
   render() {
     return (
       <div className='city' key={this.props.key}>
-        <h5>{this.props.name}</h5>
-        <h6>{this.props.country}</h6>
+        <img id="thumbnailLeft" src="../images/thumbnail.jpg" alt='thumbnail' />
+        <section>
+          <h5>{this.props.name}</h5>
+          <h6>{this.props.country}</h6>
+        </section>
       </div>
     );
   }
--- src/components/Post.js
+++ src/components/Post.js
@@ -4,9 +4,12 @@ import React, { Component } from 'react';
 class Post extends Component {
   render() {
     return (
-      <div key={this.props.key}>
-        <h3>{this.props.title}</h3>
-        <h4>{this.props.description}</h4>
+      <div id="postWrap" key={this.props.key}>
+        <img id="thumbnail" src="../images/thumbnail.jpg" alt='thumnail' />
+        <section id="eachPost">
+          <p id="postTitle">{this.props.title}</p>
+          <p id="postDes">{this.props.description}</p>
+        </section>
       </div>
     );
   }
