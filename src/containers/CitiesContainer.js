1a709c22cc57b669ca5e0189dfa1b80229df2786
Srishti Sehtia <srishti0123@gmail.com>
2018-04-03 15:59:33 -0700

    complete axios requests

--- src/containers/CitiesContainer.js
+++ src/containers/CitiesContainer.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import CityList from '../components/CityList';
 import CityView from '../components/CityView';
 import CityModel from '../models/City'
+import PostModel from '../models/Post';
+
 
 class CitiesContainer extends Component {
   constructor() {
@@ -12,9 +14,19 @@ class CitiesContainer extends Component {
       city: {}
     }
     this.populateCityView = this.populateCityView.bind(this)
+    this.createPost = this.createPost.bind(this);
   }
   componentDidMount(){
     this.fetchData()
+  }
+
+  createPost(newPost, cityID) {
+    PostModel.create(newPost, cityID).then( (res) => {
+      let currPosts = this.state.posts
+      currPosts.push(res.data)
+      this.setState({posts: currPosts})
+      console.log(currPosts)
+    })
   }
   fetchData(){
     CityModel.all().then( (res) => {
@@ -34,7 +46,7 @@ class CitiesContainer extends Component {
     return (
       <div class="row">
         <CityList cities={this.state.cities} populateCityView={this.populateCityView}/>
-        <CityView city={this.state.city} posts={this.state.posts}/>
+        <CityView city={this.state.city} posts={this.state.posts} createPost={this.createPost}/>
       </div>
     );
   }
